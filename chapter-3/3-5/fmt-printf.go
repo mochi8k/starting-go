913c3c94e25c206d0bbffdedddf39dd2deb92650
mochi8k <y.hkh.185@gmail.com>
2016-10-12 19:22:34 +0900

    add fmt-printf

---
+++ chapter-3/3-5/fmt-printf.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Printf("数値=%d\n", 5)
+
+	fmt.Printf("10進数=%d 2進数=%b 8進数=%o 16進数=%x\n", 17, 17, 17, 17)
+
+	fmt.Printf("%d年%d月%d日\n", 2015, 12, 15)
+}
